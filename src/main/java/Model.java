230bc5d22ad3d518b133ee123d480e761d07014a
nasim_aust <nasim5153@gmail.com>
2018-10-10 22:42:19 +0600

    Commit for github

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -19,8 +19,7 @@ public class Model {
 
     public Model() throws Exception {
         try {
-            connect = DriverManager.getConnection("jdbc:mysql://b313c62a518843:e5f66dce@us-cdbr-iron-east-01.cleardb.net/heroku_81c6003783d7d01?reconnect=true");
-            //connect = DriverManager.getConnection("jdbc:mysql://localhost/"+DBName+"?user="+this.DBUser+"&password="+this.DBPassword);
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/"+DBName+"?user="+this.DBUser+"&password="+this.DBPassword);
         } catch (Exception e) {
             throw e;
         }
